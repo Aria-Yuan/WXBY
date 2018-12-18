c358d28f4d0ca2cb8e3707d3151e53bb6d25514a
浩然 吕 <lvhr547@gmail.com>
2018-12-18 12:22:35 +0800

    wuwuwuwuwwuweishenmelhrzhemelajia

--- app/src/main/java/com/example/joan/myapplication/database/model/BaseModel.java
+++ app/src/main/java/com/example/joan/myapplication/database/model/BaseModel.java
@@ -9,8 +9,8 @@ public class BaseModel {
 //    final static public String IP_ADDR = "140.136.155.131";//实验室
 //    final static public String IP_ADDR = "192.168.137.126";//楊冠宇的WiFi
 //    final static public String IP_ADDR = "140.136.28.71";
-    final static public String IP_ADDR = "192.168.1.107";
-//    final static public String IP_ADDR = "192.168.3.102";
+//    final static public String IP_ADDR = "192.168.1.107";
+    final static public String IP_ADDR = "192.168.137.1";
 //    final static public String IP_ADDR = "140.136.22.101";
 
 }
